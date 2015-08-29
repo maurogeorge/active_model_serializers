289cce428c63f75698a83e6bda5e01b7b216db09
Mauro George <maurogot@gmail.com>
2015-08-29 12:37:24 -0300

    Create assert_response_schema test helper
    It is a common pattern to use JSON Schema to validate a API response[1], [2]
    and [3].
    
    This patch creates the `assert_response_schema` test helper that helps people do
    this kind of validation easily on the controller tests.
    
    [1]: https://robots.thoughtbot.com/validating-json-schemas-with-an-rspec-matcher
    [2]: https://github.com/sharethrough/json-schema-rspec
    [3]: https://github.com/rails-api/active_model_serializers/issues/1011#issuecomment-127608121

---
+++ test/assertions_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+module ActiveModel
+  class Serializer
+    class AssertionsTest < ActionController::TestCase
+      class MyController < ActionController::Base
+        def index
+          render json: Profile.new(name: 'Name 1', description: 'Description 1', comments: 'Comments 1')
+        end
+
+        def show
+          index
+        end
+      end
+
+      tests MyController
+
+      def test_that_assert_with_a_valid_schema
+        get :index
+        assert_response_schema
+      end
+
+      def test_that_raises_a_json_schema_with_a_invalid_schema
+        get :show
+        assert_raises JSON::Schema::ValidationError do
+          assert_response_schema
+        end
+      end
+
+      def test_that_assert_with_a_custom_schema
+        get :show
+        assert_response_schema('custom/show.json')
+      end
+
+      def test_that_assert_with_a_custom_schema_directory
+        original_schema_path = ActiveModel::Serializer.config.schema_path
+        ActiveModel::Serializer.config.schema_path = 'test/support/custom_schemas'
+
+        get :index
+        assert_response_schema
+
+        ActiveModel::Serializer.config.schema_path = original_schema_path
+      end
+    end
+  end
+end
