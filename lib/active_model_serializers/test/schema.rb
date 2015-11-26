5e4b7bd97bea33393e1a4b943cc45f6ce470b718
Mauro George <maurogot@gmail.com>
2015-11-26 19:28:38 -0200

    Create AssertResponseSchema to handle the assert_response_schema

--- lib/active_model_serializers/test/schema.rb
+++ lib/active_model_serializers/test/schema.rb
@@ -3,14 +3,43 @@ require 'json-schema'
 module ActiveModelSerializers
   module Test
     module Schema
-
       def assert_response_schema(schema_path = nil)
-        controller_path = response.request.filtered_parameters[:controller]
-        action = response.request.filtered_parameters[:action]
-        schema_directory = ActiveModel::Serializer.config.schema_path
-        schema_path ||= "#{controller_path}/#{action}.json"
-        schema_full_path = "#{schema_directory}/#{schema_path}"
-        JSON::Validator.validate!(schema_full_path, response.body, strict: true)
+        AssertResponseSchema.new(schema_path, response).call
+      end
+
+      class AssertResponseSchema
+        attr_reader :schema_path, :response
+
+        def initialize(schema_path, response)
+          @response = response
+          @schema_path = schema_path || schema_path_default
+        end
+
+        def call
+          JSON::Validator.validate!(schema_full_path, response.body, strict: true)
+        end
+
+        private
+
+        def controller_path
+          response.request.filtered_parameters[:controller]
+        end
+
+        def action
+          response.request.filtered_parameters[:action]
+        end
+
+        def schema_directory
+          ActiveModel::Serializer.config.schema_path
+        end
+
+        def schema_full_path
+          "#{schema_directory}/#{schema_path}"
+        end
+
+        def schema_path_default
+          "#{controller_path}/#{action}.json"
+        end
       end
     end
   end
